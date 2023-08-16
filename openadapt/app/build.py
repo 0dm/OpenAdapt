f8ceb3afd0ff5c50ff6825d989d39bf5788ec124
Aaron <57018940+0dm@users.noreply.github.com>
2023-08-15 20:21:13 -0400

    add cffi

--- openadapt/app/build.py
+++ openadapt/app/build.py
@@ -11,32 +11,42 @@ import subprocess
 
 import nicegui
 
-spec = [
-    "pyi-makespec",
-    f"{Path(__file__).parent / 'tray.py'}",
-    f"--icon={Path(__file__).parent / 'assets' / 'logo.ico'}",
-    "--name",
-    "OpenAdapt",  # name
-    # "--onefile", # trade startup speed for smaller file size
-    "--onedir",
-    "--windowed",  # prevent console appearing, only use with ui.run(native=True, ...)
-    "--add-data",
-    f"{Path(nicegui.__file__).parent}:{Path('nicegui')}",
-    "--add-data",
-    f"{Path(__file__).parent}:{Path('assets')}",
-]
 
-subprocess.call(spec)
+def generate_spec_file():
+    spec = [
+        "pyi-makespec",
+        f"{Path(__file__).parent / 'tray.py'}",
+        f"--icon={Path(__file__).parent / 'assets' / 'logo.ico'}",
+        "--name",
+        "OpenAdapt",  # name
+        # "--onefile",  # trade startup speed for smaller file size
+        "--onedir",
+        "--windowed",  # prevent console appearing, only use with ui.run(native=True, ...)
+        "--add-data",
+        f"{Path(nicegui.__file__).parent}:{Path('nicegui')}",
+        "--add-data",
+        f"{Path(__file__).parent}:{Path('assets')}",
+    ]
 
-# add import sys ; sys.setrecursionlimit(sys.getrecursionlimit() * 5)
-#  to line 2 of OpenAdapt.spec
-with open("OpenAdapt.spec", "r+") as f:
-    lines = f.readlines()
-    lines[1] = "import sys ; sys.setrecursionlimit(sys.getrecursionlimit() * 5)\n"
-    f.seek(0)
-    f.truncate()
-    f.writelines(lines)
+    subprocess.call(spec)
 
-# building
-proc = subprocess.Popen("pyinstaller OpenAdapt.spec", shell=True)
-proc.wait()
+
+def start():
+    generate_spec_file()
+
+    # add import sys ; sys.setrecursionlimit(sys.getrecursionlimit() * 5)
+    #  to line 2 of OpenAdapt.spec
+    with open("OpenAdapt.spec", "r+") as f:
+        lines = f.readlines()
+        lines[1] = "import sys ; sys.setrecursionlimit(sys.getrecursionlimit() * 5)\n"
+        f.seek(0)
+        f.truncate()
+        f.writelines(lines)
+
+    # building
+    proc = subprocess.Popen("pyinstaller OpenAdapt.spec", shell=True)
+    proc.wait()
+
+
+if __name__ == "__main__":
+    start()
